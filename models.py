30916968fcf6096b6e97e060d2fbd0e2654e00c2
Shy-Li <72149457+Shy-Li@users.noreply.github.com>
2022-09-13 11:38:01 -0500

    final version for publication

--- models.py
+++ models.py
@@ -8,19 +8,18 @@ import torch
 import torch.nn as nn
 
 class TarToPert(nn.Module):
-    def __init__(self,neck):
+    def __init__(self):
         super(TarToPert, self).__init__()
-        self.neck = neck
         self.lay1 = nn.Sequential(
             nn.Linear(252, 256),
             nn.ReLU(True), 
             ) 
         self.lay2 = nn.Sequential(
-            nn.Linear(256, neck),
+            nn.Linear(256, 128),
             nn.ReLU(True), 
             ) 
         self.lay3 = nn.Sequential(
-            nn.Linear(neck, 256),
+            nn.Linear(128, 256),
             nn.ReLU(True), 
             )
         self.lay4 =  nn.Linear(256, 252)
